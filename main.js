5aa5e446cffb7edf43e0d362c5e39853178a9145
maulanasolehudn <solehudinmaulana75@gmail.com>
2022-08-22 09:30:00 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,674 @@
+
+function clOff (){
+    document.getElementById("light").style.background="yellow";
+   }
+   function clIff (){
+    document.getElementById("light").style.background="";  
+}
+
+function clI (){
+    document.getElementById("o").style.background="yellow";
+   }
+   function clU (){
+    document.getElementById("o").style.background="";  
+}
+
+function clAa (){
+    document.getElementById("a").style.background="yellow";
+   }
+   function clAaa (){
+    document.getElementById("a").style.background="";  
+}
+
+function clIii (){
+    document.getElementById("ii").style.background="yellow";
+   }
+   function clIiii (){
+    document.getElementById("ii").style.background="";  
+}
+
+function clUuu (){
+    document.getElementById("uu").style.background="yellow";
+   }
+   function clUuuu (){
+    document.getElementById("uu").style.background="";  
+}
+
+function clOoo (){
+    document.getElementById("oo").style.background="yellow";
+   }
+   function clOooo (){
+    document.getElementById("oo").style.background="";  
+}
+function clEe (){
+    document.getElementById("e").style.background="yellow";
+   }
+   function clEee (){
+    document.getElementById("e").style.background="";  
+}
+
+function clAee (){
+    document.getElementById("ae").style.background="yellow";
+   }
+   function clAeee (){
+    document.getElementById("ae").style.background="";  
+}
+
+function clAii (){
+    document.getElementById("ai").style.background="yellow";
+   }
+   function clAiii (){
+    document.getElementById("ai").style.background="";  
+}
+
+function clCc (){
+    document.getElementById("c").style.background="yellow";
+   }
+   function clCcc (){
+    document.getElementById("c").style.background="";  
+}
+
+function clCaa (){
+    document.getElementById("ca").style.background="yellow";
+   }
+   function clCaaa (){
+    document.getElementById("ca").style.background="";  
+}
+
+function clKamu (){
+    document.getElementById("aku").style.background="yellow";
+   }
+   function clKita (){
+    document.getElementById("aku").style.background="";  
+}
+function clAkan (){
+    document.getElementById("dia").style.background="yellow";
+   }
+   function clPergi (){
+    document.getElementById("dia").style.background="";  
+}
+function clSudah (){
+    document.getElementById("kami").style.background="yellow";
+   }
+   function clBerjanji (){
+    document.getElementById("kami").style.background="";  
+}
+function clBakso (){
+    document.getElementById("makan").style.background="yellow";
+   }
+   function clBakar (){
+    document.getElementById("makan").style.background="";  
+}
+function clTusuk (){
+    document.getElementById("sate").style.background="yellow";
+   }
+   function clYamete (){
+    document.getElementById("sate").style.background="";  
+}
+function clMandir (){
+    document.getElementById("mondar").style.background="yellow";
+   }
+   function clMandor (){
+    document.getElementById("mondar").style.background="";  
+}
+function clBik (){
+    document.getElementById("bak").style.background="yellow";
+   }
+   function clBuk (){
+    document.getElementById("bak").style.background="";  
+}
+function clBrik (){
+    document.getElementById("brak").style.background="yellow";
+   }
+   function clBruk (){
+    document.getElementById("brak").style.background="";  
+}
+function clBrok (){
+    document.getElementById("brek").style.background="yellow";
+   }
+   function clBrook (){
+    document.getElementById("brook").style.background="";  
+}
+function clBret (){
+    document.getElementById("brat").style.background="yellow";
+   }
+   function clBrot (){
+    document.getElementById("brat").style.background="";  
+}
+function clErt (){
+    document.getElementById("art").style.background="yellow";
+   }
+   function clOrt (){
+    document.getElementById("art").style.background="";  
+}
+function clUrt (){
+    document.getElementById("irt").style.background="yellow";
+   }
+   function clUrtt (){
+    document.getElementById("irt").style.background="";  
+}
+function clNgik (){
+    document.getElementById("ngak").style.background="yellow";
+   }
+   function clNguk (){
+    document.getElementById("ngak").style.background="";  
+}
+function clNgok (){
+    document.getElementById("ngek").style.background="yellow";
+   }
+   function clNgook (){
+    document.getElementById("ngek").style.background="";  
+}
+function clCic (){
+    document.getElementById("cac").style.background="yellow";
+   }
+   function clCuc (){
+    document.getElementById("cac").style.background="";  
+}
+function clCoc (){
+    document.getElementById("cec").style.background="yellow";
+   }
+   function clCooc (){
+    document.getElementById("cec").style.background="";  
+}
+function clIc (){
+    document.getElementById("ac").style.background="yellow";
+   }
+   function clUc (){
+    document.getElementById("ac").style.background="";  
+}
+
+function clCaiiii (){
+    document.getElementById("caaaa").style.background="yellow";
+   }
+   function clCauuu (){
+    document.getElementById("caaaa").style.background="";  
+}
+function clBab (){
+    document.getElementById("baa").style.background="yellow";
+   }
+   function clBac (){
+    document.getElementById("baa").style.background="";  
+}
+function clBae (){
+    document.getElementById("bad").style.background="yellow";
+   }
+   function clBaf (){
+    document.getElementById("bad").style.background="";  
+}
+function clBabi (){
+    document.getElementById("baba").style.background="yellow";
+   }
+   function clBabu (){
+    document.getElementById("baba").style.background="";  
+}
+function clBabo (){
+    document.getElementById("babe").style.background="yellow";
+   }
+   function clBaboo (){
+    document.getElementById("babe").style.background="";  
+}
+function clBir (){
+    document.getElementById("bar").style.background="yellow";
+   }
+   function clBur (){
+    document.getElementById("bar").style.background="";  
+}
+function clBit (){
+    document.getElementById("bat").style.background="yellow";
+   }
+   function clBut (){
+    document.getElementById("bat").style.background="";  
+}
+function clBot (){
+    document.getElementById("bet").style.background="yellow";
+   }
+   function clBoot (){
+    document.getElementById("bet").style.background="";  
+}
+function clCik (){
+    document.getElementById("cak").style.background="yellow";
+   }
+   function clCuk (){
+    document.getElementById("cak").style.background="";  
+}
+function clCok (){
+    document.getElementById("cek").style.background="yellow";
+   }
+   function clCook (){
+    document.getElementById("cek").style.background="";  
+}
+function clCrik (){
+    document.getElementById("crak").style.background="yellow";
+   }
+   function clCruk (){
+    document.getElementById("crak").style.background="";  
+}
+function clCrok (){
+    document.getElementById("crek").style.background="yellow";
+   }
+   function clCrook (){
+    document.getElementById("crek").style.background="";  
+}
+function clDik (){
+    document.getElementById("dak").style.background="yellow";
+   }
+   function clDuk(){
+    document.getElementById("dak").style.background="";  
+}
+function clDok (){
+    document.getElementById("dek").style.background="yellow";
+   }
+   function clDook(){
+    document.getElementById("dek").style.background="";  
+}
+function clDiik (){
+    document.getElementById("daak").style.background="yellow";
+   }
+   function clDuuk(){
+    document.getElementById("daak").style.background="";  
+}
+function clDoook (){
+    document.getElementById("deek").style.background="yellow";
+   }
+   function clDooook(){
+    document.getElementById("deek").style.background="";  
+}
+function clZz (){
+    document.getElementById("z").style.background="yellow";
+   }
+   function clZzz(){
+    document.getElementById("z").style.background="";  
+}
+function clYy (){
+    document.getElementById("y").style.background="yellow";
+   }
+   function clYyy(){
+    document.getElementById("y").style.background="";  
+}
+function clWw (){
+    document.getElementById("w").style.background="yellow";
+   }
+   function clWww(){
+    document.getElementById("w").style.background="";  
+    
+}
+function clVv (){
+    document.getElementById("v").style.background="yellow";
+   }
+   function clVvv(){
+    document.getElementById("v").style.background="";  
+}
+function clXx (){
+    document.getElementById("x").style.background="yellow";
+   }
+   function clXxx(){
+    document.getElementById("x").style.background="";  
+}
+function clMamang (){
+    document.getElementById("momi").style.background="yellow";
+   }
+   function clModol (){
+    document.getElementById("momi").style.background="";  
+}
+
+function clTt (){
+    document.getElementById("t").style.background="yellow";
+   }
+   function clTtt(){
+    document.getElementById("t").style.background="";  
+}
+
+
+function clJj (){
+    document.getElementById("j").style.background="yellow";
+   }
+   function clJjj(){
+    document.getElementById("j").style.background="";  
+}
+
+function clKk (){
+    document.getElementById("k").style.background="yellow";
+   }
+   function clKkk(){
+    document.getElementById("k").style.background="";  
+}
+
+function clLl (){
+    document.getElementById("l").style.background="yellow";
+   }
+   function clLll(){
+    document.getElementById("l").style.background="";  
+}
+
+function clMm (){
+    document.getElementById("m").style.background="yellow";
+   }
+   function clMmm(){
+    document.getElementById("m").style.background="";  
+}
+
+function clNn (){
+    document.getElementById("n").style.background="yellow";
+   }
+   function clNnn(){
+    document.getElementById("n").style.background="";  
+}
+
+function clButut (){
+    document.getElementById("mobil").style.background="yellow";
+   }
+   function clKaluaran(){
+    document.getElementById("mobil").style.background="";  
+}
+
+function clBaheula (){
+    document.getElementById("zaman").style.background="yellow";
+   }
+   function clAsekk(){
+    document.getElementById("zaman").style.background="";  
+}
+
+
+function clSuzuki (){
+    document.getElementById("honda").style.background="yellow";
+   }
+   function clAvanza(){
+    document.getElementById("honda").style.background="";  
+}
+
+function clHatori(){
+    document.getElementById("ninja").style.background="yellow";
+   }
+   function clDingin(){
+    document.getElementById("ninja").style.background="";  
+}
+function clToyota(){
+    document.getElementById("kawasaki").style.background="yellow";
+   }
+   function clSupra(){
+    document.getElementById("kawasaki").style.background="";  
+}
+
+function clKursi(){
+    document.getElementById("meja").style.background="yellow";
+   }
+   function clLaptop(){
+    document.getElementById("meja").style.background="";  
+}
+function clAkuu(){
+    document.getElementById("selalu").style.background="yellow";
+   }
+   function clLihat(){
+    document.getElementById("selalu").style.background="";  
+}
+function clPunggung(){
+    document.getElementById("belakang").style.background="yellow";
+   }
+   function clMu(){
+    document.getElementById("belakang").style.background="";  
+}
+function clKau(){
+    document.getElementById("disaat").style.background="yellow";
+   }
+   function clLihaat(){
+    document.getElementById("disaat").style.background="";  
+}
+function clPunggungg(){
+    document.getElementById("belakangg").style.background="yellow";
+   }
+   function clPria(){
+    document.getElementById("belakangg").style.background="";  
+}
+function clHowoo(){
+    document.getElementById("lain").style.background="yellow";
+   }
+   function clWoo(){
+    document.getElementById("lain").style.background="";  
+}
+function clSalahku(){
+    document.getElementById("apa").style.background="yellow";
+   }
+   function clApaa(){
+    document.getElementById("apa").style.background="";  
+}
+function clIbuku(){
+    document.getElementById("salah").style.background="yellow";
+   }
+   function clHidup(){
+    document.getElementById("salah").style.background="";  
+}
+function clRundung(){
+    document.getElementById("di").style.background="yellow";
+   }
+   function clWibu(){
+    document.getElementById("di").style.background="";  
+}
+function clGelo(){
+    document.getElementById("anjay").style.background="yellow";
+   }
+   function clAngjay(){
+    document.getElementById("anjay").style.background="";  
+}
+function clRatna(){
+    document.getElementById("oh").style.background="yellow";
+   }
+   function clKenapa(){
+    document.getElementById("oh").style.background="";  
+}
+function clPilih(){
+    document.getElementById("lo").style.background="yellow";
+   }
+   function clIrfan(){
+    document.getElementById("lo").style.background="";  
+}
+function clBiji(){
+    document.getElementById("padahal").style.background="yellow";
+   }
+   function clNya(){
+    document.getElementById("padahal").style.background="";  
+}
+function clMuka(){
+    document.getElementById("kapalan").style.background="yellow";
+   }
+   function clKayak(){
+    document.getElementById("kapalan").style.background="";  
+}
+function clWuh(){
+    document.getElementById("banteng").style.background="yellow";
+   }
+   function clBantengg(){
+    document.getElementById("banteng").style.background="";  
+}
+function clRatnaa(){
+    document.getElementById("ohh").style.background="yellow";
+   }
+   function clKenapaa(){
+    document.getElementById("ohh").style.background="";  
+}
+
+function clPiliih(){
+    document.getElementById("lu").style.background="yellow";
+   }
+   function clirpan(){
+    document.getElementById("lu").style.background="";  
+}
+function clBijinya(){
+    document.getElementById("paadahal").style.background="yellow";
+   }
+   function clKapalaan(){
+    document.getElementById("paadahal").style.background="";  
+}
+function clCuma(){
+    document.getElementById("luu").style.background="yellow";
+   }
+   function cGak(){
+    document.getElementById("luu").style.background="";  
+}
+function clSama(){
+    document.getElementById("suka").style.background="yellow";
+   }
+   function cGue(){
+    document.getElementById("suka").style.background="";  
+}
+function clCumaa(){
+    document.getElementById("luuu").style.background="yellow";
+   }
+   function cGakk(){
+    document.getElementById("luuu").style.background="";  
+}
+function clSamaa(){
+    document.getElementById("mau").style.background="yellow";
+   }
+   function cGuee(){
+    document.getElementById("mau").style.background="";  
+}
+function clKenapaa(){
+    document.getElementById("tapi").style.background="yellow";
+   }
+   function clIirpan(){
+    document.getElementById("tapi").style.background="";  
+}
+function clEway(){
+    document.getElementById("luh").style.background="yellow";
+   }
+   function clKontlo(){
+    document.getElementById("luh").style.background="";  
+}
+function clBangsul(){
+    document.getElementById("anjim").style.background="yellow";
+   }
+   function clKontrak(){
+    document.getElementById("anjim").style.background="";  
+}
+function clDuudi(){
+    document.getElementById("dudi").style.background="yellow";
+   }
+   function clDam(){
+    document.getElementById("dudi").style.background="";  
+}
+function clDudii(){
+    document.getElementById("duudii").style.background="yellow";
+   }
+   function clDamm(){
+    document.getElementById("duudii").style.background="";  
+}
+function clIs(){
+    document.getElementById("what").style.background="yellow";
+   }
+   function clYou(){
+    document.getElementById("what").style.background="";  
+}
+function clIam(){
+    document.getElementById("eat").style.background="yellow";
+   }
+   function clThe(){
+    document.getElementById("eat").style.background="";  
+}
+function clToo(){
+    document.getElementById("chef").style.background="yellow";
+   }
+   function clCoocking(){
+    document.getElementById("chef").style.background="";  
+}
+function clTempe(){
+    document.getElementById("tahu").style.background="yellow";
+   }
+   function clIkan(){
+    document.getElementById("tahu").style.background="";  
+}
+function clDaging(){
+    document.getElementById("telur").style.background="yellow";
+   }
+   function clAyam(){
+    document.getElementById("telur").style.background="";  
+}
+function clBeruang(){
+    document.getElementById("bebek").style.background="yellow";
+   }
+   function clCobra(){
+    document.getElementById("bebek").style.background="";  
+}
+function clSusu(){
+    document.getElementById("kopi").style.background="yellow";
+   }
+   function clJahe(){
+    document.getElementById("kopi").style.background="";  
+}
+function clLang(){
+    document.getElementById("bela").style.background="yellow";
+   }
+   function clSem(){
+    document.getElementById("bela").style.background="";  
+}
+function clBelalang(){
+    document.getElementById("bah").style.background="yellow";
+   }
+   function clSembah(){
+    document.getElementById("bah").style.background="";  
+}
+function clHentuy(){
+    document.getElementById("anime").style.background="yellow";
+   }
+   function clYknts(){
+    document.getElementById("anime").style.background="";  
+}
+function clKny(){
+    document.getElementById("jjk").style.background="yellow";
+   }
+   function clAot(){
+    document.getElementById("jjk").style.background="";  
+}
+function clIsi(){
+    document.getElementById("bakpau").style.background="yellow";
+   }
+   function clTai(){
+    document.getElementById("bakpau").style.background="";  
+}
+function clNurul(){
+    document.getElementById("smk").style.background="yellow";
+   }
+   function clIslam(){
+    document.getElementById("smk").style.background="";  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
